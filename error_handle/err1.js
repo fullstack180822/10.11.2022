94c10314e1020675b946f9d8217ad96743afcf06
micserv010821 <dotnet290320@gmail.com>
2022-11-10 18:49:57 +0200

    upload all

---
+++ error_handle/err1.js
@@ -0,0 +1,18 @@
+
+function divive(x, y) {
+    if (y == 0) {
+        console.warn('cannot divie by zero!')
+    }
+}
+
+let input_number = 'a'
+let number = parseInt(input_number)
+const details = { 
+    first_name : 'itay',
+    // name : {
+    //     first_name : 'itay'
+    // }
+}
+console.log(details.name.first_name);
+console.log('here??????');
+
---
+++ targil1.js
@@ -0,0 +1,24 @@
+
+// 1
+// in nodejs -- run a method called a()
+// see it crashes
+// add try catch to the call and in the catch print 'method does not exist' + the js error object [parameter in the catch]
+// add finally which prints -- 'finally code- after method a'
+// add another print after the finally 
+// create function a() -- which does nothing 
+// run again the code, did the finally print was executed
+
+// 2
+// create HTML page with button
+// open the console 
+// filter out the errors 
+// onclick the button into function click1
+// inside function click1 call method a() 
+// run the page -- look in the console. see the error? why?
+// restore the filter , run again
+// add try catch to the call and in the catch print 'method does not exist' + the js error object [parameter in the catch]
+// add finally which prints -- 'finally code- after method a'
+// add another print after the finally 
+// create function a() -- which does nothing 
+// run again the code, did the finally print was executed
+// can you click the button many times?
